a53a9876f64cf8deb0deb29f22d7ed63d8e5d3c1
Hai Lin Zhang <hzhan339@hotmail.com>
2018-10-14 20:56:36 -0700

    basic

---
+++ classes/chessPiece.py
@@ -0,0 +1,9 @@
+class chessPiece:
+    def __init__(self,isAlive, color, turns):
+        self.isAlive = isAlive
+        self.color = color
+        self.turns = turns
+
+    def getPosition(self):
+    def _validMove(self):
+    def moveToPosition(self):

ab9ac465064335c7d336cad74fbd24ce92fa1053
Hai Lin Zhang <hzhan339@hotmail.com>
2018-10-31 20:19:35 -0700

    attacked, protected done

--- classes/AI/AI_controller.py
+++ classes/AI/AI_controller.py
@@ -15,7 +15,7 @@ class VanillaBoardAI:
                + self.calculate_heuristic_score(piece, new_col, new_row)
 
     def calculate_attacks_and_protects_score(self, piece, col, row):
-        return self.number_pieces_attacked(piece, col, row) + self.number_pieces_protected(piece, col, row)
+        return self.number_pieces_attacked_and_protected(piece, col, row)
 
     # Heuristic is based on number of pieces close to enemy King - adopts a mobbing strategy to win game
     def calculate_heuristic_score(self, piece, col, row):
@@ -28,80 +28,397 @@ class VanillaBoardAI:
     def is_check(self):
         return False
 
-    # Note: only considers pieces in close vicinity / attacking range of the piece!
-    def number_pieces_attacked(self, piece, col, row):
+    # Returns [#attacked, #protected]
+    def number_pieces_attacked_and_protected(self, piece, col, row):
         attacked = 0
+        protected = 0
         # PAWN
         if piece == 2 or piece == 3:
             if piece == 2:
-                if self.is_black_piece(row-1, col+1):
-                    attacked += 1
-                if self.is_black_piece(row-1, col-1):
-                    attacked += 1
+                current_score = self._handle_white_move(row - 1, col + 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_white_move(row - 1, col - 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
             elif piece == 3:
-                if self.is_white_piece(row+1, col+1):
-                    attacked += 1
-                if self.is_white_piece(row+1, col-1):
-                    attacked += 1
+                current_score = self._handle_black_move(row + 1, col + 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_black_move(row + 1, col - 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
         # KING
         elif piece == 4 or piece == 5:
-            # TODO: finish attacking logic, do protecting
             if piece == 4:
-                if self.is_black_piece(row-1, col-1):
-                    attacked += 1
-                if self.is_black_piece(row-1, col):
-                    attacked += 1
-                if self.is_black_piece(row-1, col+1):
-                    attacked += 1
-                if self.is_black_piece(row, col-1):
-                    attacked += 1
-                if self.is_black_piece(row, col+1):
-                    attacked += 1
-                if self.is_black_piece(row+1, col-1):
-                    attacked += 1
-                if self.is_black_piece(row+1, col):
-                    attacked += 1
-                if self.is_black_piece(row+1, col+1):
-                    attacked += 1
+                current_score = self._handle_white_move(row - 1, col - 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_white_move(row - 1, col)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_white_move(row - 1, col + 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_white_move(row, col - 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_white_move(row, col + 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_white_move(row + 1, col - 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_white_move(row + 1, col)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_white_move(row + 1, col + 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
             elif piece == 5:
-                if self.is_white_piece(row-1, col-1):
-                    attacked += 1
-                if self.is_white_piece(row-1, col):
-                    attacked += 1
-                if self.is_white_piece(row-1, col+1):
-                    attacked += 1
-                if self.is_white_piece(row, col-1):
-                    attacked += 1
-                if self.is_white_piece(row, col+1):
-                    attacked += 1
-                if self.is_white_piece(row+1, col-1):
-                    attacked += 1
-                if self.is_white_piece(row+1, col):
-                    attacked += 1
-                if self.is_white_piece(row+1, col+1):
-                    attacked += 1
+                current_score = self._handle_black_move(row - 1, col - 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_black_move(row - 1, col)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_black_move(row - 1, col + 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_black_move(row, col - 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_black_move(row, col + 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_black_move(row + 1, col - 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_black_move(row + 1, col)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_black_move(row + 1, col + 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
         # QUEEN
         elif piece == 6 or piece == 7:
-            if piece == 6:
-                return self._bishop_attacks(col, row) + self._rook_attacks(col, row)
+            bishop_score = self._handle_bishop_move(col, row, piece)
+            attacked += bishop_score[0]
+            protected += bishop_score[1]
+            rook_score = self._handle_rook_move(col, row, piece)
+            attacked += rook_score[0]
+            protected += rook_score[1]
+        # ROOK
+        elif piece == 8 or piece == 9:
+            rook_score = self._handle_rook_move(col, row, piece)
+            attacked += rook_score[0]
+            protected += rook_score[1]
+        # BISHOP
+        elif piece == 10 or piece == 11:
+            bishop_score = self._handle_bishop_move(col, row, piece)
+            attacked += bishop_score[0]
+            protected += bishop_score[1]
+        # KNIGHT
+        elif piece == 12 or piece == 13:
+            if piece == 12:
+                current_score = self._handle_white_move(row, col + 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_white_move(row, col - 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_white_move(row + 1, col)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_white_move(row - 1, col)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_white_move(row + 1, col + 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_white_move(row + 1, col - 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_white_move(row - 1, col + 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_white_move(row - 1, col - 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+            elif piece == 13:
+                current_score = self._handle_black_move(row, col + 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_black_move(row, col - 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_black_move(row + 1, col)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_black_move(row - 1, col)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_black_move(row + 1, col + 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_black_move(row + 1, col - 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_black_move(row - 1, col + 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+                current_score = self._handle_black_move(row - 1, col - 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+        return [attacked, protected]
 
-        return attacked
-
-    def number_pieces_protected(self, piece, col, row):
-        return 0
-
-    # False if out of bounds
-    def is_black_piece(self, row, col):
+    # Returns -1 if protected, 1 if attacked, 0 otherwise
+    def _handle_white_move(self, row, col):
         if row > 7 or col > 7 or row < 0 or col < 0:
-            return False
+            return 0
         board = self._board_object.get_board()
         piece = board[row][col]
-        return piece != 0 and piece % 2 == 1
+        if piece != 0 and piece % 2 == 1:
+            return 1
+        elif piece != 0 and piece % 2 == 0:
+            return -1
+        else:
+            return 0
 
-    # False if out of bounds
-    def is_white_piece(self, row, col):
+    # Returns -1 if protected, 1 if attacked, 0 otherwise
+    def _handle_black_move(self, row, col):
         if row > 7 or col > 7 or row < 0 or col < 0:
-            return False
+            return 0
         board = self._board_object.get_board()
         piece = board[row][col]
-        return piece != 0 and piece % 2 == 0
+        if piece != 0 and piece % 2 == 0:
+            return 1
+        elif piece != 0 and piece % 2 == 1:
+            return -1
+        else:
+            return 0
+
+    # Returns [#attacked, #protected]
+    def _handle_bishop_move(self, col, row, piece):
+        current_score = 0
+        attacked = 0
+        protected = 0
+        board = self._board_object.get_board()
+        while board[row+1][col+1]:
+            if board[row+1][col+1] != 0:
+                if piece == 8:
+                    current_score = self._handle_white_move(row + 1, col + 1)
+                elif piece == 9:
+                    current_score = self._handle_black_move(row + 1, col + 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+            row += 1
+            col += 1
+        while board[row+1][col-1]:
+            if board[row+1][col-1] != 0:
+                if piece == 8:
+                    current_score = self._handle_white_move(row + 1, col - 1)
+                elif piece == 9:
+                    current_score = self._handle_black_move(row + 1, col - 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+            row += 1
+            col -= 1
+        while board[row-1][col+1]:
+            if board[row-1][col+1] != 0:
+                if piece == 8:
+                    current_score = self._handle_white_move(row - 1, col + 1)
+                elif piece == 9:
+                    current_score = self._handle_black_move(row - 1, col + 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+            row -= 1
+            col += 1
+        while board[row-1][col-1]:
+            if board[row-1][col-1] != 0:
+                if piece == 8:
+                    current_score = self._handle_white_move(row - 1, col - 1)
+                elif piece == 9:
+                    current_score = self._handle_black_move(row - 1, col - 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+            row -= 1
+            col -= 1
+        return [attacked, protected]
+
+
+    # Returns [#attacked, #protected]
+    def _handle_rook_move(self, col, row, piece):
+        current_score = 0
+        attacked = 0
+        protected = 0
+        board = self._board_object.get_board()
+        while board[row][col + 1]:
+            if board[row][col + 1] != 0:
+                if piece == 8:
+                    current_score = self._handle_white_move(row, col + 1)
+                elif piece == 9:
+                    current_score = self._handle_black_move(row, col + 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+            col += 1
+        while board[row + 1][col]:
+            if board[row + 1][col] != 0:
+                if piece == 8:
+                    current_score = self._handle_white_move(row + 1, col)
+                elif piece == 9:
+                    current_score = self._handle_black_move(row + 1, col)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+            row += 1
+        while board[row][col - 1]:
+            if board[row][col - 1] != 0:
+                if piece == 8:
+                    current_score = self._handle_white_move(row, col - 1)
+                elif piece == 9:
+                    current_score = self._handle_black_move(row, col - 1)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+            col -= 1
+        while board[row - 1][col]:
+            if board[row - 1][col] != 0:
+                if piece == 8:
+                    current_score = self._handle_white_move(row - 1, col)
+                elif piece == 9:
+                    current_score = self._handle_black_move(row - 1, col)
+                if current_score != 0:
+                    if current_score == 1:
+                        attacked += 1
+                    else:
+                        protected += 1
+            row -= 1
+        return [attacked, protected]
